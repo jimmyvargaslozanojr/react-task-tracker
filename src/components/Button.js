75a4b880126a6d345408a3ea795b784eefb9ce7a
jimmyvargaslozanojr <zirymmij@gmail.com>
2021-04-09 12:19:09 +0800

    function

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,6 @@
 import PropTypes from 'prop-types'
 
-const Button = ({color, text}) => {
-
-    const onClick = () => {
-        console.log('clicked')
-    }
+const Button = ({color, text, onClick}) => {
 
     return (
         <button onClick={onClick} 
@@ -20,7 +16,8 @@ Button.defaultProps ={
 
 Button.protoTypes = {
     text: PropTypes.string,
-    color: PropTypes.string
+    color: PropTypes.string,
+    onClick: PropTypes.func.isRequired,
 
 }
 
